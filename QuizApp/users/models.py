10b3b9024800579df74c27f31ad0f9ecce50f626
Maansee <maansee313@gmail.com>
2021-03-07 22:37:39 +0530

    created otp functionality

--- QuizApp/users/models.py
+++ QuizApp/users/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-
+from django.contrib.auth.models import User
 
 
 class Profile(models.Model):
@@ -13,7 +13,7 @@ class Profile(models.Model):
         return f'{self.user.username} Profile'
 
 
-    def save(self):
+    def save(self,*args,**kwargs):
         super().save()
 
     class Meta:
@@ -25,3 +25,15 @@ def update_user_profile(sender, instance, created, **kwargs):
     if created:
         Profile.objects.create(user=instance)
     instance.profile.save()
+
+
+
+class UserOTP(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    time_st = models.DateTimeField(auto_now=True)
+    otp = models.SmallIntegerField()
+
+
+
+
+
--- QuizApp/users/views.py
+++ QuizApp/users/views.py
@@ -1,16 +1,67 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from .forms import UserRegisterForm
+from django.contrib.auth.models import User
+import random
+from .models import UserOTP
+from .import forms
+from django.core.mail import send_mail
+from django.conf import settings
 
 
 def register(request):
     if request.method == 'POST':
+
+        #verify otp
+        otp_present = request.POST.get('otp')
+
+        if otp_present:
+            get_user = request.POST.get('usr')
+            usr = User.objects.get(username=get_user)
+            if not usr.is_active:
+                if int(otp_present)== UserOTP.objects.filter(user = usr).last().otp:
+                    messages.success(request, f'Account created for {username}')
+                    return redirect('quiz-home')
+                else:
+                     messages.error(request, f'Account not created for {username}. Please validate OTP')
+                     return render(request, 'user/signup.html', {'otp': True, 'usr': usr})
+
+
+
         form = UserRegisterForm(request.POST)
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
-            messages.success(request, f'Account created for {username}')
-            return redirect('quiz-home')
+            
+
+
+            usr = User.objects.get(username=username)
+            usr.is_active = False
+            usr.email = usr.email
+            usr.save()   #cannot login until otp is entered
+
+            usr_otp = random.randint(10000, 999999)
+
+            #creating user otp here
+            UserOTP.objects.create(user=usr, otp = usr_otp)
+
+            #creating mail msg
+            msg = f"Hi {username}, \n Your OTP is {usr_otp}."
+
+            #create an email
+            send_mail(
+                "Welcome to ExamFunda. Verify your email.",
+                msg,
+                settings.EMAIL_HOST_USER,
+                [usr.email],
+                fail_silently = False
+                )
+
+            return render(request, 'users/register.html', {'otp':True, 'usr':usr})
+            
+
+
+             
     else:
         form = UserRegisterForm()
     return render(request, 'users/register.html', {'form': form})
